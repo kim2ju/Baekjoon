fda320c69810a282caded29f1b084ab6ef546d10
kim2ju <84396632+kim2ju@users.noreply.github.com>
2022-02-11 00:32:31 +0900

    Create 10828.py

---
+++ python/silver/10828.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for i in range(N):
+  order = input().split()
+  if order[0] == 'push':
+    stack.append(order[1])
+
+  elif order[0] == 'pop':
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack.pop())
+
+  elif order[0] == 'size':
+    print(len(stack))
+
+  elif order[0] == 'empty':
+    if len(stack) == 0:
+      print(1)
+    else:
+      print(0)
+
+  else:
+    if len(stack) == 0:
+      print(-1)
+    else:
+      print(stack[-1])
