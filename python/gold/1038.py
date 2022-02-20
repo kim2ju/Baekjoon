8b58e33bd579d2650abd4a092a85989b813586d2
kim2ju <84396632+kim2ju@users.noreply.github.com>
2022-02-21 02:09:45 +0900

    Update 1038.py

--- python/gold/1038.py
+++ python/gold/1038.py
@@ -8,10 +8,8 @@ num_list = []
 
 def findDecreaseNum() :
   for i in range(1, 11):
-    for combination in combinations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], i):
-      combination = list(combination)
-      combination.reverse()
-      num_list.append(int(''.join(map(str, combination))))
+    for combination in combinations(['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'], i):
+      num_list.append(int(''.join(combination)))
 
   num_list.sort()
 
