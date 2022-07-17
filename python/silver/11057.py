897524b530148fcb436d0c04c25049e6af0cac8f
kim2ju <84396632+kim2ju@users.noreply.github.com>
2022-07-17 12:15:53 +0900

    Create 11057.py

---
+++ python/silver/11057.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+dp = [[0] * 10 for _ in range(N+1)]
+dp[1] = [1] * 10
+
+for i in range(2, N+1):
+  dp[i][0] = dp[i-1][0]
+  dp[i][1] = dp[i][0] + dp[i-1][1]
+  dp[i][2] = dp[i][1] + dp[i-1][2]
+  dp[i][3] = dp[i][2] + dp[i-1][3]
+  dp[i][4] = dp[i][3] + dp[i-1][4]
+  dp[i][5] = dp[i][4] + dp[i-1][5]
+  dp[i][6] = dp[i][5] + dp[i-1][6]
+  dp[i][7] = dp[i][6] + dp[i-1][7]
+  dp[i][8] = dp[i][7] + dp[i-1][8]
+  dp[i][9] = dp[i][8] + dp[i-1][9]
+
+print(sum(dp[N]) % 10007)
